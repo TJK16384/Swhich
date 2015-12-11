e6ad23b10a954369b8c727b172272712d8ff8f80
Dragero <stevenj747@gmail.com>
2015-12-11 18:51:31 -0500

    Fixed Send Function (and formatting)
    I may have missed a semicolon

--- js/client.js
+++ js/client.js
@@ -5,42 +5,42 @@ var JSONdata = [{
 	"id":0,
 	"scheduleActive": true,
 	"Schedule": [
-           {"state": "ON", "datetime": "08:00:00"}]
+		{"state": "ON", "datetime": "08:00:00"}]
 	},{},{},{}]
 */ //JSON file format for reference
 
 function update(){
 	switch(mode.value){ 
 	case "normal":
-	
-	 if (JSONdata[ID.value]){ //If there is an object for the current ID
-	/* for (var x in JSONdata[ID.value]["Schedule"]){
-		 if ()
-	 }*/
-	 
-	  JSONdata[ID.value]["Schedule"].push({state:"ON", datetime: clock.value});
-			JSONdata[ID.value]["scheduleActive"] = true;}
+		
+		if (JSONdata[ID.value]){ //If there is an object for the current ID
+			/* for (var x in JSONdata[ID.value]["Schedule"]){
+		if ()
+	}*/
 			
-	 else{ //If there is no object, create it
-	  JSONdata.push({"id":ID.value, "scheduleActive": true, "Schedule": [{state:"ON", datetime: clock.value}]});}
+			JSONdata[ID.value]["Schedule"].push({state:"ON", datetime: clock.value});
+			JSONdata[ID.value]["scheduleActive"] = true;}
+		
+		else{ //If there is no object, create it
+			JSONdata.push({"id":ID.value, "scheduleActive": true, "Schedule": [{state:"ON", datetime: clock.value}]});}
 		break;
 		
 	case "on":
-	
-	 if (JSONdata[ID.value]){ //If there is an object for the current ID
-	  JSONdata[ID.value]["Schedule"].push({state:"ON", datetime: clock.value});
-		 JSONdata[ID.value]["scheduleActive"] = false;}
-	 else{ //If there is no object, create it
-		JSONdata.push({"id":ID.value, "scheduleActive": false, "Schedule": [{state:"ON", datetime: "00:00"}]});}
+		
+		if (JSONdata[ID.value]){ //If there is an object for the current ID
+			JSONdata[ID.value]["Schedule"].push({state:"ON", datetime: clock.value});
+			JSONdata[ID.value]["scheduleActive"] = false;}
+		else{ //If there is no object, create it
+			JSONdata.push({"id":ID.value, "scheduleActive": false, "Schedule": [{state:"ON", datetime: "00:00"}]});}
 		break;
 		
 	case "off":
-	
-	if (JSONdata[ID.value]){ //If there is an object for the current ID
-	  JSONdata[ID.value]["Schedule"].push({state:"OFF", datetime: clock.value});
-		 JSONdata[ID.value]["scheduleActive"] = false;}
-	 else{ //If there is no object, create it
-		JSONdata.push({"id":ID.value, "scheduleActive": false, "Schedule": [{state:"OFF", datetime: "00:00"}]});}
+		
+		if (JSONdata[ID.value]){ //If there is an object for the current ID
+			JSONdata[ID.value]["Schedule"].push({state:"OFF", datetime: clock.value});
+			JSONdata[ID.value]["scheduleActive"] = false;}
+		else{ //If there is no object, create it
+			JSONdata.push({"id":ID.value, "scheduleActive": false, "Schedule": [{state:"OFF", datetime: "00:00"}]});}
 		break;
 
 	}
@@ -49,16 +49,17 @@ function update(){
 }
 function send(){
 	JSONdata = JSON.parse(output.value);
- $.post("./update", JSONdata)
-
- console.log("Data sent");
+	$.post("./update", JSONdata, function() {
+		console.log("Data sent");
+		);
+	}
 }
 function load(){
- $.get("DATA.json", function(DATA){
- 
- JSONdata = DATA;
- output.value= JSON.stringify(JSONdata);
- });
- console.log("Data Recieved");
+	$.get("DATA.json", function(DATA){
+		JSONdata = DATA;
+		output.value= JSON.stringify(JSONdata);
+		console.log("Data Recieved");
+	});
 }
+
 $(document).ready(function(){load();}); //Loads the file from the server when the page loads
